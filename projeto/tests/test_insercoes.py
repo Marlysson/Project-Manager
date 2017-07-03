f50d1a9d7bf9bc79622f39740f621583b6b8d5aa
Marlysson Silva <marlysson5@gmail.com>
2017-07-03 12:04:42 -0300

    Concluindo mais uma fase de testes.

--- projeto/tests/test_insercoes.py
+++ projeto/tests/test_insercoes.py
@@ -2,20 +2,20 @@ from django.test import TestCase
 from datetime import timedelta
 
 from ..models import Projeto, Funcao, Funcionario, \
-                    Endereco, Tarefa, Checklist
+                    Endereco, Tarefa, Checklist, \
+                    Equipe, Participacao
 
 class TestInsercoesModelos(TestCase):
 
     def setUp(self):
         self.projeto = Projeto.objects.create(nome="Gateway Pagamentos")
-        self.funcao = Funcao.objects.create(nome="Backend Developer").save()
-        self.funcionario = Funcionario(nome="Marlysson",idade=21,
-                                salario=2.500)
-
         self.tarefa = Tarefa.objects.create(titulo="Implementar Gateway de serviço",
                         descricao="Lorem ipsum",
                         esforco=timedelta(days=4),
                         status=2,projeto=self.projeto)   
+
+        self.endereco = Endereco.objects.create(rua="Rua 1",bairro="Bairro 1",
+                    cidade="Cidade 1",estado="Estado 1")
 
     def test_deve_retornar_quantidade_de_projetos_inseridos(self):
 
@@ -32,21 +32,18 @@ class TestInsercoesModelos(TestCase):
 
         funcoes = Funcao.objects.all().count()
 
-        self.assertEquals(funcoes,4)
+        self.assertEquals(funcoes,3)
 
     def test_deve_criar_um_funcionario_e_associar_um_endereco_a_ele(self):
 
-        endereco = Endereco.objects.create(rua="Rua 1",bairro="Bairro 1",
-                            cidade="Cidade 1",estado="Estado 1")
+        funcionario = Funcionario.objects.create(nome="Marlysson",idade=21,
+                                salario=2.500,endereco=self.endereco)
 
-        self.funcionario.endereco = endereco
-        self.funcionario.save()
-
-        funcionario_banco = Funcionario.objects.get(id=self.funcionario.id)
+        funcionario_banco = Funcionario.objects.get(id=funcionario.id)
 
         self.assertEquals(funcionario_banco.nome,"Marlysson")
 
-    def test_criar_uma_checklist_para_uma_tarefa(self):
+    def test_criar_uma_checklist_com_itens_para_uma_tarefa(self):
         
         checklist = Checklist.objects.create(descricao="Primeiros passos para integrar api",tarefa=self.tarefa)
         checklist.addItem("Fazer busca inicial de requisitos")
@@ -58,3 +55,38 @@ class TestInsercoesModelos(TestCase):
         self.assertEquals(checklist_inserida.descricao,"Primeiros passos para integrar api")
 
         self.assertEquals(checklist_inserida.itens.count(),3)
+
+    def test_deve_criar_uma_equipe_para_um_projeto_associando_seus_participantes(self):
+
+        equipe = Equipe.objects.create(nome="Desenvolvimento",projeto=self.projeto)
+
+        funcao_backend = Funcao.objects.create(nome="Backend Developer")
+        funcao_frontend = Funcao.objects.create(nome="FrontEnd Developer")
+
+        funcionario_marlysson = Funcionario.objects.create(nome="Marlysson",idade=21,
+                                salario=2.500,endereco=self.endereco)
+
+        endereco_2 = Endereco.objects.create(rua="Rua 2",bairro="Bairro 2",
+            cidade="Cidade 2",estado="Estado 2")
+
+        funcionario_marcelo = Funcionario.objects.create(nome="Marcelo",idade=20,
+                                salario=2.600,endereco=endereco_2)
+
+        marlysson = {
+            "equipe":equipe,
+            "funcionario":funcionario_marlysson,
+            "funcao":funcao_backend
+        }
+
+        marcelo = {
+            "equipe":equipe,
+            "funcionario":funcionario_marcelo,
+            "funcao":funcao_frontend
+        }
+
+        participacao_1 = Participacao.objects.create(**marlysson)
+        participacao_2 = Participacao.objects.create(**marcelo)
+
+        equipe = Equipe.objects.get(id=1)
+
+        self.assertEquals(equipe.participantes.count(),2)
