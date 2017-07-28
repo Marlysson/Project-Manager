ec435d6ca7bfb8df5fccaa50194806fcf14247be
Marlysson <marlysson5@gmail.com>
2017-07-28 01:59:49 -0300

    Adicioando funcionalidade de inserção de tarefa com seus atributos.. alteração de layout.

--- projeto/urls.py
+++ projeto/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
 	
 	url(r"^projetos$",views.projetos,name="projetos"),
 	url(r"^funcionarios$",views.funcionarios,name="funcionarios"),
+	url(r"^tarefas$",views.tarefas,name="tarefas"),
+	url(r"^tarefa/(?P<id_tarefa>\d+)$",views.tarefa,name="tarefa"),
 
 	url(r"^novo_projeto$",views.novo_projeto,name="novo_projeto"),
 	url(r"^novo_funcionario$",views.novo_funcionario,name="novo_funcionario"),
+	url(r"^nova_tarefa$",views.nova_tarefa,name="nova_tarefa"),
 ]
--- projeto/views.py
+++ projeto/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
-from .models import Projeto, Funcionario
+from .models import Projeto, Funcionario, Tarefa
 from .forms import FuncionarioForm
 
 def index(request):
@@ -37,6 +37,47 @@ def novo_funcionario(request):
 
 	if novo_funcionario.is_valid():
 		novo_funcionario.save()
-		print("entrou")
 
 	return redirect("funcionarios")
+
+def tarefas(request):
+
+	tarefas = Tarefa.objects.all()
+	projetos = Projeto.objects.all()
+	funcionarios = Funcionario.objects.all()
+
+	contexto = {"tarefas":tarefas,"projetos":projetos,"funcionarios":funcionarios}
+
+	return render(request,"tarefas.html",contexto)
+
+def tarefa(request,id_tarefa):
+	pass
+	
+def nova_tarefa(request):
+
+	if request.method == 'POST':
+		
+		titulo = request.POST.get("titulo").capitalize()
+		descricao = request.POST.get("descricao").capitalize()
+		projeto = request.POST.get("projeto")
+		responsavel = request.POST.get("funcionario")
+		pre_requisitos = request.POST.getlist("pre_requisito")
+
+		ids_requisitos = list(map(int,pre_requisitos))
+
+		projeto = Projeto.objects.get(id=projeto)
+		responsavel = Funcionario.objects.get(id=responsavel)
+
+		tarefa = Tarefa(titulo=titulo,descricao=descricao,
+						projeto=projeto,responsavel=responsavel)
+		tarefa.save()
+
+		if len(ids_requisitos):
+
+			for id_requisito in ids_requisitos:
+				requisito = Tarefa.objects.get(id=id_requisito)
+
+				tarefa.pre_requisitos.add(requisito)
+
+
+	return redirect("tarefas")
